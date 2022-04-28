c2e1cfe968354e5f4c8c4bb4b12581c0d25de35c
hasparus <hasparus@gmail.com>
2022-04-28 13:57:59 +0200

    Remove autoFocus from Search

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,21 +1,20 @@
-import * as React from 'react'
+import * as React from "react";
 
-import './search-styles.scss'
+import "./search-styles.scss";
 
 type Props = {
-  placeHolderText?: string
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-}
+  placeHolderText?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-export const Search = ({placeHolderText = '', onChange}: Props) => (
+export const Search = ({ placeHolderText = "", onChange }: Props) => (
   <div className="search">
     <input
       className="search-input"
       aria-label="Search input"
       type="search"
-      placeholder={placeHolderText ? placeHolderText : 'Search'}
-      autoFocus
+      placeholder={placeHolderText ? placeHolderText : "Search"}
       onChange={onChange}
     />
   </div>
-)
+);
